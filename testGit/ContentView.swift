eed0fd3c19e7d3f229a031532b615b7e887c824d
VadimKuida <vadimkuida@gmail.com>
2020-08-03 10:30:49 +0300

    Initial Commit

---
+++ testGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testGit
+//
+//  Created by Вадим Куйда on 03.08.2020.
+//  Copyright © 2020 Вадим Куйда. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
